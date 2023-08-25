3be34125b53196f8ff37c7aca6a319703c95d4a0
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2023-08-25 17:42:11 -0600

    remove custom font from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,12 +19,6 @@ export default function Home() {
     <>
         <Head>
             <title>Matt Folsom Portfolio</title>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link
-                href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
-                rel="stylesheet"
-            />
         </Head>
         <nav id="desktop-nav" className="desktop-nav">
             <div className="logo">Matt Folsom</div>

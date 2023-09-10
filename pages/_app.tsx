60cf176cdcdb205ab19f7803b61c9040bdf6deda
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2023-09-10 14:33:37 -0600

    resolve mobile 6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
       <>
         <Head>
-          <meta name="viewport" content="viewport-fit=cover, initial-scale=1.0"/>
+          <meta name="viewport" content="viewport-fit=cover"/>
         </Head>
         <Component {...pageProps} />
       </>

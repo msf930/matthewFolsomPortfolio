3044a3b59431ec447443d6a38964b834ad6e7a90
mfolsom930 <86228082+mfolsom930@users.noreply.github.com>
2023-09-10 14:08:37 -0600

    mobile resolve 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
       <>
         <Head>
-          <meta name="viewport" content="viewport-fit=cover" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <Component {...pageProps} />
       </>
